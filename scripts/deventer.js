9ed6124ad345cb990e82a3028348cc8d32d83751
dagothar <dagothar@gmail.com>
2017-05-13 13:19:44 +0200

    initial structure

---
+++ scripts/deventer.js
@@ -0,0 +1,11 @@
+define(['threejs'], function(threejs) {
+
+  function Deventer() {
+
+  };
+
+
+  return {
+    Deventer: Deventer
+  };
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,28 @@
 "use strict";
 
-$(document).ready(function() {
-  
-  var app = new App();
-  app.run();
-  
+requirejs.config({
+  baseUrl: 'scripts',
+  paths: {
+    jquery: '../vendor/jquery/jquery-3.2.0.min',
+    threejs: '../vendor/threejs/three.min',
+    underscore: '../vendor/underscore/underscore.min'
+  },
+  shim: {
+    jquery: {
+      exports: '$'
+    },
+    underscore: {
+      exports: '_'
+    }
+  }
+});
+
+
+requirejs(['app'], function(app) {
+
+  $(document).ready(function() {
+    var application = new app.App();
+    application.run();
+  });
+
 });
